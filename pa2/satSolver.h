ba61f7b27b0380ccc4c77d4ca4123e38e34b0a35
Weiqiang Yu <w8yu@uwaterloo.ca>
2022-12-17 19:15:41 -0500

    memory_issue

--- pa2/satSolver.h
+++ pa2/satSolver.h
@@ -2,6 +2,7 @@
 #define SAT_H
 
 #include <vector>
+#include <memory>
 
 bool satCallingMiniSat(unsigned int numVar, std::vector<std::vector<int>> &cnf); // return the SAT result of cnf by calling MiniSAT
 
